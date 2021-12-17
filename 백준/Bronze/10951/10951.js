02cb009ae4b61e5886387c20662d91ec8a54868e
WoosubLeee <zbxv1423@naver.com>
2021-12-18 02:51:42 +0900

    solve on 211217

---
+++ 백준/Bronze/10951/10951.js
@@ -0,0 +1,27 @@
+// const readline = require('readline');
+// const fs = require('fs');
+
+// const rl = readline.createInterface({
+//   input: fs.createReadStream('input.txt')
+// });
+
+// rl.on('line', line => {
+//   let input = line.toString().split(' ');
+//   let a = parseInt(input[0]);
+//   let b = parseInt(input[1]);
+
+//   console.log(a+b);
+// })
+
+//
+
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().trim().split('\n');
+
+for (let i = 0; i < input.length; i++) {
+  const line = input[i].split(' ');
+  let a = parseInt(line[0]);
+  let b = parseInt(line[1]);
+
+  console.log(a+b);
+}
--- 백준/Bronze/10952.py
+++
@@ -1,7 +0,0 @@
-while True:
-    a, b = map(int, input().split())
-
-    if a and b:
-        print(a + b)
-    else:
-        break
